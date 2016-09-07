28fcb06b12178f749d89395bd2ea7e0e1b062334
Guillaume Sabran <sabranguillaume@gmail.com>
2016-09-06 20:04:23 -0700

    init

---
+++ Example/Pods/Target Support Files/GLMatrix/GLMatrix-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_GLMatrix : NSObject
+@end
+@implementation PodsDummy_GLMatrix
+@end
---
+++ Example/Pods/Target Support Files/Pods-GLMatrix_Tests/Pods-GLMatrix_Tests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_GLMatrix_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_GLMatrix_TestsVersionString[];
+
